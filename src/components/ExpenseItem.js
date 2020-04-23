7ae1ca542fd922ec220e9b2f103977ecea60dd0a
rehanshk2017 <rehanshk2017@gmail.com>
2020-04-23 12:32:56 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { MdEdit, MdDelete } from "react-icons/md";
+const ExpenseItem = ({ expense, handleEdit, handleDelete }) => {
+  const { id, charge, amount } = expense;
+  return (
+    <li className="item">
+      <div className="info">
+        <span className="expense">{charge}</span>
+        <span className="amount">${amount}</span>
+      </div>
+      <button
+        onClick={() => handleEdit(id)}
+        className="edit-btn"
+        aria-label="edit button"
+      >
+        <MdEdit />
+      </button>
+      <button
+        onClick={() => handleDelete(id)}
+        className="clear-btn"
+        aria-label="delete button"
+      >
+        <MdDelete />
+      </button>
+    </li>
+  );
+};
+
+export default ExpenseItem;
